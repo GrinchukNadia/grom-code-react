f9b1dea6fb58f9fbc85d26a46b783d5ca6a2278a
Надежда <grennad991@gmail.com>
2022-02-06 18:24:38 +0200

    worked with react and jsx

---
+++ 01-react_intro/03_task/src/index.js
@@ -0,0 +1,19 @@
+import './styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.querySelector('#root');
+
+const renderGreeting = (elem) => {
+
+  const jsxElem = (
+    <div className='greeting'>
+      <div className='greeting__title'>Hello, world!</div>
+      <div className='greeting__text'>I'm learning React</div>
+    </div>
+  );
+
+  ReactDOM.render(jsxElem, elem)
+};
+
+renderGreeting(rootElement);
