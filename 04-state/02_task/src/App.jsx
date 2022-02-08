32947c968fcbdb430847b5f61a762da11996771f
Надежда <grennad991@gmail.com>
2022-02-08 20:02:54 +0200

    worked with state, class components, moment js

---
+++ 04-state/02_task/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Clock from "./Clock.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Clock
+        location={'New York'}
+        offset={0}
+        />
+      <Clock
+        location={'Kyiv'}
+        offset={2}
+        />
+      <Clock
+        location={'London'}
+        offset={-5}
+      />
+    </>
+  )
+}
+
+export default App;
