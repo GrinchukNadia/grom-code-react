06edf616e2c0b076003ca90d5ea471c70eb2c566
Надежда <grennad991@gmail.com>
2022-02-08 17:12:12 +0200

    worked with functional components, props and momentJS

---
+++ 03-components_props/05_task/src/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import moment from 'moment';
+import './profile.scss';
+
+const editDate = date => moment(date).format('DD MMM YY')
+
+const Profile = (props) => {
+  return (
+    <div className='profile'>
+      <div className='profile__name'>{`${props.userData.firstName} ${props.userData.lastName}`}</div>
+      <div className='profile__birth'>{`was born ${editDate(
+        props.userData.birthDate
+      )} in ${props.userData.birthPlace}`}</div>
+    </div>
+  );
+}
+
+export default Profile;
