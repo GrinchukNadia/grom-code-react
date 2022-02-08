aee543fff5e52280f1c8601b4f7ff6517273f1eb
Надежда <grennad991@gmail.com>
2022-02-08 15:18:47 +0200

    worked with small functional commponents and props

--- 03-components_props/03_task/src/Avatar.jsx
+++ 03-components_props/03_task/src/Avatar.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './avatar.scss';
 
 function Avatar(props) {
-  console.log(props)
   return (
     <img
       className='avatar'
-      src={props.user.avatarUrl}
-      alt={props.user.name}
+      src={props.avatarUrl}
+      alt={props.name}
     />
   );
 }
