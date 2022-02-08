fbb5984ab2890aacf4aae2ecb5b35dd9542b5256
Надежда <grennad991@gmail.com>
2022-02-08 15:25:23 +0200

    worked with small functional commponents and props

--- 03-components_props/03_task/src/index.jsx
+++ 03-components_props/03_task/src/index.jsx
@@ -4,16 +4,18 @@ import "./styles.scss"
 import Comment from "./Comment.jsx"
 
 const author = {
-  user: {
-    name: 'Tom tom',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/9919?s=460&v=4',
-  },
-  text: 'Some text',
-  date: new Date(),
+  name: 'Tom tom',
+  avatarUrl: 'https://avatars.githubusercontent.com/u/9919?s=460&v=4',
 };
+const text = 'Some text';
+const date = new Date();
 
 const rootElem = document.querySelector('#root');
 ReactDOM.render(
-  <Comment author={author} />,
+  <Comment 
+    author={author}
+    text={text}
+    date={date} 
+  />,
   rootElem
 );
