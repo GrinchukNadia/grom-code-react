30aef669b765b9df98c8d609956309ae6050eeff
Надежда <grennad991@gmail.com>
2022-02-09 13:04:33 +0200

    worked with class component, props and event handlers

--- 05-events/04_task/src/Colors.jsx
+++ 05-events/04_task/src/Colors.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import './colors.scss';
 
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
 class Colors extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      color: props.color
-    }
   }
 
-  onClickColor() {
-    document.body.style.backgroundColor = this.state.color; 
+  onClickColor(color) {
+    document.body.style.backgroundColor = color; 
   }
 
   render() {
     return (
-      <button 
-        className='colors__button'
-        style={{backgroundColor: this.state.color}}
-        onClick={() => this.onClickColor()}
-      ></button>
-    )
+      <div className='colors'>
+        <button
+          className='colors__button'
+          style={{ backgroundColor: RED }}
+          onClick={() => this.onClickColor(RED)}
+        ></button>
+
+        <button
+          className='colors__button'
+          style={{ backgroundColor: GREEN }}
+          onClick={() => this.onClickColor(GREEN)}
+        ></button>
+
+        <button
+          className='colors__button'
+          style={{ backgroundColor: BLUE }}
+          onClick={() => this.onClickColor(BLUE)}
+        ></button>
+      </div>
+    );
   }
 }
 
--- 05-events/04_task/src/index.jsx
+++ 05-events/04_task/src/index.jsx
@@ -3,16 +3,10 @@ import ReactDOM from "react-dom";
 import Colors from "./Colors.jsx";
 import "./index.scss"
 
-const RED ="#f00";
-const GREEN ="#0f0";
-const BLUE ="#00f";
-
 const rootElem = document.querySelector('#root');
 ReactDOM.render(
   <>
-    <Colors color={RED} />
-    <Colors color={GREEN} />
-    <Colors color={BLUE} />
+    <Colors />
   </>,
   rootElem
 );
