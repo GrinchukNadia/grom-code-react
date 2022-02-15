7ec2d58aae9cb2f6263156168248ba114bc7bc03
Надежда <grennad991@gmail.com>
2022-02-15 12:59:08 +0200

    worked with lifecycle methods fethc and github api

--- 08-lifecycle_methods/04_task/src/User.jsx
+++ 08-lifecycle_methods/04_task/src/User.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './user.scss';
 
-const User = ({ name, age }) => {
-  return (
-    <li className='user'>
-      <span className='user__name'>{name}</span>
-      <span className='user__age'>{age}</span>
-    </li>
-  );
+class User extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://api.github.com/users/${this.props.userId}`)
+      .then(response => response.json())
+      .then(user => this.setState({user}))
+  }
+
+  render() {
+    if(!this.state.user) {
+      return null
+    }
+
+    const user = this.state.user
+
+    return (
+      <div className='user'>
+        <img
+          alt='User Avatar'
+          src={user.avatar_url}
+          className='user__avatar'
+        />
+        <div className='user__info'>
+          <span className='user__name'>{user.name}</span>
+          <span className='user__location'>{user.location}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default User;
--- 08-lifecycle_methods/04_task/src/index.jsx
+++ 08-lifecycle_methods/04_task/src/index.jsx
@@ -1,55 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import UsersList from './UsersList.jsx';
+import User from './User.jsx';
 import './index.scss';
 
-const users = [
-  {
-    id: 'id-0',
-    age: 21,
-    name: 'Bob',
-  },
-  {
-    id: 'id-1',
-    age: 17,
-    name: 'Tom',
-  },
-  {
-    id: 'id-2',
-    age: 18,
-    name: 'Tad',
-  },
-  {
-    id: 'id-3',
-    age: 45,
-    name: 'Sam',
-  },
-  {
-    id: 'id-4',
-    age: 39,
-    name: 'Gorge',
-  },
-  {
-    id: 'id-5',
-    age: 22,
-    name: 'Hue',
-  },
-  {
-    id: 'id-6',
-    age: 56,
-    name: 'Bill',
-  },
-  {
-    id: 'id-7',
-    age: 61,
-    name: 'Henry',
-  },
-  {
-    id: 'id-8',
-    age: 19,
-    name: 'Dik',
-  },
-];
+const userId = 'Facebook';
 
 const rootElem = document.querySelector('#root');
-ReactDOM.render(<UsersList users={users} />, rootElem);
+ReactDOM.render(<User userId={userId} />, rootElem);
