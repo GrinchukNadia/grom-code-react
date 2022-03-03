26f7992f69f16b5cac087f675c9d12ca7068cbfa
Надежда <grennad991@gmail.com>
2022-03-03 09:56:35 +0200

    filterable list items

---
+++ 10-lifting_state_up/04_task/src/FilterableProductTable.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar.jsx';
+import ProductTable from './ProductTable.jsx';
+
+class FilterableProductTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filterText: '',
+      inStockOnly: false,
+    };
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText,
+    });
+  }
+
+  handleInStockChange(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly,
+    });
+  }
+
+  render() {
+    const {filterText, inStockOnly} = this.state;
+
+    return (
+      <div>
+        <SearchBar
+          filterText={filterText}
+          inStockOnly={inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={filterText}
+          inStockOnly={inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+export default FilterableProductTable;
