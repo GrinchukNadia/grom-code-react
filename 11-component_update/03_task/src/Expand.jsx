be903960c0604e7b7d39c32e11fe686be7c6c032
Надежда <grennad991@gmail.com>
2022-03-07 12:13:31 +0200

    refactore

--- 11-component_update/03_task/src/Expand.jsx
+++ 11-component_update/03_task/src/Expand.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Expand = ({ children, onHide, title, isWisible }) => {
+const Expand = ({ children, onHide, isWisible, title }) => {
 
   return (
     <div className='expand border'>
       <div className='expand__header'>
         <span className='expand__title'>{title}</span>
         <button onClick={onHide} className='expand__toggle-btn'>
-          <i className='fas fa-chevron-up'></i>
+            <i style={isWisible ? null : {transform: 'rotate(180deg)'} } className='fas fa-chevron-up'></i>
         </button>
       </div>
       {isWisible ? <div className='expand__content'>{children}</div> : null}
