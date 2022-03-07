82ca9c62665985bdd00b0adf26743a0811711b73
Надежда <grennad991@gmail.com>
2022-03-07 12:18:00 +0200

    refactoring

--- 11-component_update/03_task/src/App.jsx
+++ 11-component_update/03_task/src/App.jsx
@@ -4,23 +4,32 @@ import Expand from './Expand.jsx';
 
 class App extends Component {
   state = {
-    isWisible: false
-  }
+    isWisible: false,
+  };
 
   onHide = () => {
     this.setState({
-      isWisible: !this.state.isWisible
-    })
-  }
+      isWisible: !this.state.isWisible,
+    });
+  };
 
   render() {
     return (
       <div className='app'>
-        <Expand onHide={this.onHide} isWisible={this.state.isWisible} title={'Some title'}>
-          <p>
-            Hooks are a new addition in React 16.8. They let you use state and
-            other React features without writing a class.
-          </p>
+        <Expand
+          onHide={this.onHide}
+          isWisible={this.state.isWisible}
+          title={'Some title'}
+        >
+          {this.state.isWisible ? (
+            <div className='expand__content'>
+              {' '}
+              <p>
+                Hooks are a new addition in React 16.8. They let you use state
+                and other React features without writing a class.
+              </p>
+            </div>
+          ) : null}
         </Expand>
       </div>
     );
--- 11-component_update/03_task/src/Expand.jsx
+++ 11-component_update/03_task/src/Expand.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const Expand = ({ children, onHide, isWisible, title }) => {
-
   return (
     <div className='expand border'>
       <div className='expand__header'>
         <span className='expand__title'>{title}</span>
         <button onClick={onHide} className='expand__toggle-btn'>
-            <i style={isWisible ? null : {transform: 'rotate(180deg)'} } className='fas fa-chevron-up'></i>
+          <i
+            style={isWisible ? null : { transform: 'rotate(180deg)' }}
+            className='fas fa-chevron-up'
+          ></i>
         </button>
       </div>
-      {isWisible ? <div className='expand__content'>{children}</div> : null}
+        {children}
     </div>
   );
 };
