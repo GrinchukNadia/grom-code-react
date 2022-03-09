cd8a7c31abf8e331b7af914c6ecc25692ba86c8b
Надежда <grennad991@gmail.com>
2022-03-09 12:10:57 +0200

    moved to earlier version of react-router-dom

--- 13-react_router/01_task/src/App.jsx
+++ 13-react_router/01_task/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './index.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home.jsx';
 import Contacts from './Contacts.jsx';
 import Products from './Products.jsx';
@@ -10,12 +10,20 @@ const App = () => {
   return (
     <div className='page'>
       <BrowserRouter>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/contacts' element={<Contacts />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='/*' element={<PageNotFound />} />
-        </Routes>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/contacts'>
+            <Contacts />
+          </Route>
+          <Route path='/products'>
+            <Products />
+          </Route>
+          <Route path='/*'>
+            <PageNotFound />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
